812b81d3a2da9ee290888b85613e9e72d5750adc
Paula <paulafj2108@gmail.com>
2026-03-25 17:21:34 +0100

    Init project

---
+++ src/main/java/es/uma/tesaw/proyecto_bancosol/ProyectoBancosolApplication.java
@@ -0,0 +1,13 @@
+package es.uma.tesaw.proyecto_bancosol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancosolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancosolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uma/tesaw/proyecto_bancosol/ProyectoBancosolApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.tesaw.proyecto_bancosol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancosolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
