6cbaccae3f09a4e3ab89f2727244093b011d381a
Paula <paulafj2108@gmail.com>
2026-03-25 17:22:48 +0100

    Init project

--- src/main/java/es/uma/tesaw/proyecto_bancosol/ProyectoBancosolApplication.java
+++ src/main/java/es/uma/tesaw/proyecto_bancosol/ProyectoBancosolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoBancosolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoBancosolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBancosolApplication.class, args);
+    }
 
 }
